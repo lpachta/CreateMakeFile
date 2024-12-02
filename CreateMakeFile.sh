538e0b13dcf7820f6da42c617a00080bc76d68cb
lpachta <lukin.pachta@gmail.com>
2024-12-02 12:18:14 +0100

    Added flags, gonna test

--- CreateMakeFile.sh
+++ CreateMakeFile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-help="
+HelpMenu="
   // CreateMakeFiles // 
   
   Usage:
@@ -14,43 +14,82 @@ help="
       - bin=\"<bin filename>\"
 "
 
-if [[ $1 == "-h" ]]; then
-  echo "$help"
+if [[ $# = 0 ]]; then # 0 ARGS
+  echo "$HelpMenu"
   exit
 fi
 
-if [[ $# < 1 ]]; then
-  echo "$help"
+# 1 ARG
+
+if [[ $1 = "-h" ]]; then # -h
+  echo "$HelpMenu"
   exit
 fi
-Extension=$1
+
+Extension=$1 # Loads Extension from $1
+case "$Extension" in
+"c")
+  echo "Creating Makefile for C project..."
+  ;;
+"cpp")
+  echo "Creating Makefile for C++ project..."
+  ;;
+"java")
+  echo "Creating Makefile for Java project..."
+  ;;
+*)
+  echo "Extension $Extension is not supported!"
+  echo "Aborting."
+  exit
+  ;;
+esac
 
 . ~/.config/CreateMakeFiles/CreateMakeFiles.conf # Include a config
 
-if [[ $# > 1 ]]; then
-  ProjectName=$2
-else
+if [[ config = true ]]; then
+  echo "Config was found..."
+fi
+
+while [[ $# > 0 ]]; do # 2+ ARGS
+  shift                # Extension is not $1. Working with other flags
+  case "$1" in
+  "-n" | "--Name")
+    ProjectName=$2
+    echo "Project name: $ProjectName"
+    ;;
+  "-a" | "--Author")
+    Author=$2
+    echo "Author: $Author"
+    ;;
+  "-b" | "--BinFilename")
+    Author=$2
+    echo "Bin filename: $BinFilename"
+    ;;
+  *) ;;
+  esac
+done
+
+# Defaults
+if [[ -z $ProjectName ]]; then
   ProjectName=$(basename "$(pwd)")
 fi
-
-if [[ $# > 2 ]]; then
-  bin=$3
-else
-  if [ -z "$bin" ]; then
-    bin="\$(name).bin"
-  fi
+if [[ -z $BinFilename ]]; then
+  BinFilename="\$(NAME).bin"
 fi
-
-if [ -z "$Author" ]; then
+if [[ -z $Author ]]; then
   Author=$(whoami)
 fi
+
+# All info we need from the user is: ProjectName, Author, BinFilename, Extension
+# Cannot run with no flag
+# With one flag it's -h or extension without flag
+# With more flags 1. Load the config 2. load the vars with from flags 3. Defaults
 
 for i in *.$Extension; do # Nacteni source files do array
   [ -f "$i" ] || break
   SourceFiles+=("$i")
 done
-
-if [[ ${#SourceFiles[@]} == 0 ]]; then
+if [[ ${#SourceFiles[@]} == 0 ]]; then # If no source files are found
   echo "ERROR: No files in $pwd with extension .$Extension"
   echo "Aborting"
   exit
@@ -72,7 +111,7 @@ case "$Extension" in
   Compile="\$(CC) \$(SOURCE_FILES)"
   ;;
 *)
-  echo "unsupported extension!"
+  echo "ERROR: unsupported extension!"
   exit
   ;;
 esac
@@ -85,7 +124,7 @@ NAME=$ProjectName
 SOURCE_FILES=${SourceFiles[*]}
 CC=$CC
 
-EXE_FILE=$bin 
+EXE_FILE=$BinFilename
 ALL_FILES=./*
 
 compile:
@@ -106,10 +145,17 @@ clear-pack:
 	rm \$(NAME).tar.gz
 	rm \$(NAME).zip
 
-pack: clear-bin
+pack: clear-bin 
 	rm \$(NAME)
 	tar cvzf \$(NAME).tar.gz \$(ALL_FILES)
 	zip \$(NAME).zip \$(ALL_FILES)
 
 clear: clear-bin clear-pack clear-exe
 " >Makefile
+
+if [ -f Makefile ]; then
+  echo "Makefile has been created."
+  exit
+fi
+
+echo "ERROR: Makefile has not been created."
